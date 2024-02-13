9ee569d61984d5e5d8c3c97d168ff850d0d053c0
ahmd56 <UGS.16220056@eng.suez.edu.eg>
2024-02-12 21:11:36 -0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,32 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by ahmed sallam on 17/2/2024
+//
+
+import SwiftUI
+
+/*
+ MVVM Architecture
+ 
+ Model - manages single data point
+ View - manages the UI
+ ViewModel - manages data (models) for views
+ */
+
+@main
+struct TodoListApp: App {
+    
+    @StateObject var listViewModel: ListViewModel = ListViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ListView()
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .environmentObject(listViewModel)
+        }
+    }
+}
